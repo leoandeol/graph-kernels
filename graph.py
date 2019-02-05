a6b256fe2073e6f77ee80c55ae79c685e592a19a
Léo Andéol <leo.andeol@gmail.com>
2019-02-05 22:37:44 +0100

    Mostly finished graph generator

--- graph.py
+++ graph.py
@@ -5,67 +5,107 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 from random import random
-#rajouter labels
-#melanger types graphes dans bd ?
-#pour n=1000, generer 5 types, 10 examples de chaque de taille differente et les alterer 20 fois ?
-#rajouter poids ou non dans graphe ?
-def gen_graph(type, n):
+import pickle as pk
+from time import time
+
+
+GRAPH_TYPES = ["ring", "star", "grid", "tree", "cube", "chain"]
+
+def gen_graph(type, n, nb_colors):
+    G = None
     if type == "ring":
-        return nx.cycle_graph(n)
+        G = nx.cycle_graph(n)
     elif type == "star":
-        return nx.star_graph(n)
+        G = nx.star_graph(n)
     elif type == "grid":
-        return nx.grid_graph(n)
+        G = nx.grid_2d_graph(n//2,n//2)
     elif type == "tree":
-        pass
+        G = nx.balanced_tree(2,int(np.floor(np.log2(n))))
     elif type == "cube":
-        return nx.hypercube_graph(d)
+        G = nx.hypercube_graph(int(np.floor(np.log2(n))))
     elif type == "chain":
-        pass
-    elif type == "ladder":
-        return nx.ladder_graph(n//2)
+        G = nx.path_graph(n)
     else:
         return "Error"
+    for (u,v) in G.edges():
+        G.edges[u,v]["label"] = np.random.randint(nb_colors)
+    return G
 
 #alter nodes or edges or labels ?
-def alter_graph(G, nodes):
-    G.remove_nodes(np.random.choice(G.nodes,nodes))
+def alter_graph_nodes(G, n):
+    G.remove_nodes_from(map(tuple,np.random.permutation(G.nodes())[:n]))
+                        
+def alter_graph_edges(G, n):
+    G.remove_edges_from(map(tuple,np.random.permutation(G.edges())[:n]))
+
+def alter_graph_labels(G, n):
+    for (u,v) in map(tuple,np.random.permutation(G.edges())[:n]):
+        G.edges[u,v]["label"]=np.random.randint(n)
+        
     
-def gen_and_draw(type, n):
+def gen_and_draw(type, n, quantif):
+    """
+    quantif : int
+    number of values the label can take
+    """
     nx.draw(gen_graph(type,n))
     plt.show()
 
 def product_graph(X,Y):
-	A = nx.adjacency_matrix(X)
-	B = nx.adjacency_matrix(Y)
-	W = sp.kron(A,B)
-	G = nx.from_scipy_sparse_matrix(W)
-	return G, W
-	
-def gen_database(n, nb_colours):
-	""" Generates a database of graphs
+    A = nx.adjacency_matrix(X)
+    B = nx.adjacency_matrix(Y)
+    W = sp.kron(A,B)
+    G = nx.from_scipy_sparse_matrix(W)
+    return G, W
+    
+def gen_database(nb_graphs, nb_altered, nb_nodes, nb_colors, intensity):
+    """ Generates a database of graphs
 	Parameters
 	----------
-	n : int
-		size of database
+	nb_graphs : int
+		number of random graphs
+        nb_altered : int
+                number of altered versions of a graph
+        nb_nodes : int
+                number of nodes per graph
 	nb_colours : int
 		number of possible values per colour
+    intensity : float
+          ]0;1[ intensity of alteration
 	"""
-	db_A = []
-	for i in range(n):
-		#first test
-		if random()<0.5:
-			G = gen_graph("ring", int(random()*5+10))
-			#alter_graph(G,2)
-			A_ = nx.adjacency_matrix(G).T.A
-			D = np.diag(1/np.sum(A_,0))
-			A = A_ @ D
-			db_A.append((A,"ring"))
-		else:
-			G = gen_graph("star", int(random()*5+10))
-			#alter_graph(G,2)
-			A_ = nx.adjacency_matrix(G).T.A
-			D = np.diag(1/np.sum(A_,0))
-			A = A_ @ D
-			db_A.append((A,"star"))
-	return np.array(db_A)
+    db_A = []
+    for i in range(nb_graphs):
+        #source graph
+        typ = np.random.choice(GRAPH_TYPES)
+        GS = gen_graph(typ,nb_nodes,nb_colors)
+        if GS == "Error":
+            print("Error")
+        A_ = nx.adjacency_matrix(GS).T.A
+        D = np.diag(1/np.sum(A_,0))
+        A = A_ @ D
+        db_A.append((A,typ))
+        for _ in range(nb_altered):
+            G = GS.copy()
+            alter_graph_nodes(GS,np.random.randint(max(1,int(np.floor(nb_nodes*intensity)))))
+            alter_graph_edges(GS,np.random.randint(max(1,int(np.floor(nb_nodes*intensity)))))
+            alter_graph_labels(GS,np.random.randint(max(1,int(np.floor(nb_nodes*intensity)))))
+            A_ = nx.adjacency_matrix(G).T.A
+            D = np.diag(1/np.sum(A_,0))
+            A = A_ @ D
+            db_A.append((A,typ))
+    return np.array(db_A)
+        
+def export_db(db,path):
+    with open(path,"wb") as f:
+        pk.dump(db,f)
+                
+def import_db(path):
+    with open(path,"rb") as f:
+        return pk.load(f)
+
+
+def gen_export_db(nb_graphs, nb_altered, nb_nodes, nb_colors, intensity):
+    db = gen_database(nb_graphs, nb_altered, nb_nodes, nb_colors, intensity)
+    path = str("dbs/db-"+str(time())+".dat")
+    export_db(db,path)
+    return (db,path)
