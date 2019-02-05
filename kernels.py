0d9afa804b7ed8d8b6b1ec48dc0bb5840843d742
Léo Andéol <leo.andeol@gmail.com>
2019-02-05 23:15:02 +0100

    Worked on kernel, not yet finished

--- kernels.py
+++ kernels.py
@@ -1,10 +1,17 @@
 from graph import gen_database, product_graph
 import scipy.sparse as sp
-import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
+
+import numpy as np; np.random.seed(0)
+import seaborn as sns; sns.set()
+
+from sklearn.svm import SVC
+from sklearn.utils import shuffle
+from sklearn.metrics import zero_one_loss
 
 def mu(k):
-	return 1/k
+	return 1
 
 #somme infinie ?
 # comment choisir mu, p, q
@@ -25,8 +32,22 @@ def build_gram_matrix(db):
 				gram[j, i] = ker
 	return gram
 	
-def test(n):
-	db = gen_database(n,0)
-	gram = build_gram_matrix(db[:,0])
-	plt.matshow(gram)
-	plt.show()
+def test(db):
+        #db = gen_database()
+        gram = build_gram_matrix(db[:,0])
+        #plt.matshow(gram)
+        sns.heatmap(gram)
+        plt.show()
+        n = int(gram.shape[0]*0.7)
+        X, y = shuffle(db[:,0], db[:,1])
+        print(X.shape, y.shape)
+        X_train, X_test = X[:n], X[n:]
+        y_train, y_test = y[:n], y[n:]
+        svc = SVC(kernel='precomputed')
+        
+        kernel_train = build_gram_matrix(X_train)
+        svc.fit(kernel_train, y_train)
+        # en fait np.dot(X_test, X_train.T)
+        kernel_test = build_gram_matrix(X_test)
+        y_pred = svc.predict(kernel_test)
+        print(zero_one_loss(y_test,y_pred))
