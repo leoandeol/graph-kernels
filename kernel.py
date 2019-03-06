38e4316f3732b55f5851d51403205d2f5106f4e8
Léo Andéol <leo.andeol@gmail.com>
2019-03-07 00:24:00 +0100

    Most kernels are working !

--- kernel.py
+++ kernel.py
@@ -3,16 +3,12 @@ from scipy.sparse.linalg import cg
 from scipy.linalg import solve_discrete_lyapunov
 from scipy.optimize import fixed_point
 
+#coder graph labélisés
 class Kernel:
 
     def __init__(self, lbd, N, M):
-        """
-        N = nombres de noeuds
-        N = nombre d'aretes
-        """
         self.lbd = lbd
         self.N = N
-        self.M = M
         self.mu = lambda x: pow(self.lbd,x)
 
     #pourquoi divisé par self.N
@@ -22,6 +18,7 @@ class Kernel:
         m = len(Wx.nonzero()[0])
         px = np.ones((n,1))/self.N
         qx = np.ones((n,1))/self.N
+        #diviser par le nombre d'arretes le rapproche du reste et augmente le score?
         return np.sum([self.mu(k) * qx.T @ np.power(Wx,k) @ px for k in range(self.N)])/m
 
     def inv_kernel(self, A1, A2):
@@ -31,7 +28,7 @@ class Kernel:
         m = len(Wx.nonzero()[0])
         px = np.ones((n,1))/self.N
         qx = np.ones((n,1))/self.N
-        return qx.T @ np.linalg.inv(np.identity(n)-self.lbd*Wx)@px /m
+        return qx.T @ np.linalg.inv(np.identity(n)-self.lbd*Wx)@px
 
     def sylv_eq_kernel(self, A1, A2):
         """ 
@@ -57,9 +54,11 @@ class Kernel:
          n = Wx.shape[0]
          I = np.identity(n)
          M = I-self.lbd*Wx
+         assert M.shape==Wx.shape
          px = np.ones((n,1))/self.N
          qx = np.ones((n,1))/self.N
-         x,_ = cg(M,px)
+         # donner M essentiel
+         x,_ = cg(M,px,x0=px,M=np.linalg.inv(M))
          return qx.T@x
      
     def fixed_point_kernel(self, A1, A2):
@@ -68,10 +67,15 @@ class Kernel:
         m = len(Wx.nonzero()[0])
         px = np.ones((n,1))/self.N
         qx = np.ones((n,1))/self.N
-        #ca marche
-        self.lbd = 1/(1+np.max(np.linalg.eigvals(Wx)))
+        #ca marche mais cuisine
+        self.lbd = 1/(10+np.max(np.linalg.eigvals(Wx)))
         func = lambda x: np.asarray(px+self.lbd*Wx@x)
-        x = fixed_point(func, np.asarray(px),maxiter=1000)
+        try:
+            x = fixed_point(func, np.asarray(px),maxiter=1000)
+        except:
+            print("somme en ligne",np.sum(Wx,axis=1))
+            print("somme en colonne",np.sum(Wx,axis=0))
+            print(Wx.shape,"det=",np.linalg.det(self.lbd*Wx))
         k = np.real(np.asscalar(qx.T@x))
         return k
 
